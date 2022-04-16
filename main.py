156c34522ba60786d040ae5bbb6cf1b2078b836e
Rohanop <rohankamble3011@gmail.com>
2022-04-16 22:17:30 +0530

    Fix: title to starship

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pygame.mixer.init()
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("First Game!")
+pygame.display.set_caption("Starship")
 FPS = 60
 VEL = 5
 SPACESHIP_HEIGHT, SPACESHIP_WIDTH = 30, 30
